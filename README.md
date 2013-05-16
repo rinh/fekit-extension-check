3b9b0d3a92467ac14470c90557906b2e1cc35e03
rinh <hao.lin@qunar.com>
2013-05-16 12:15:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+fekit-extension-check
+=====================
+
+### 说明 ###
+
+检查项目内容
+
+### 使用方法  ###
+
+查看帮助
+
+    fekit check --help
+
+检查当前目录下所有 .js 文件的全局变量
+
+    fekit check -g 
---
+++ index.js
@@ -0,0 +1,41 @@
+var fs = require('fs')
+var syspath = require('path')
+var baselib = syspath.join( module.parent.filename , '../' )
+var utils = require( syspath.join( baselib , 'util'  ) )
+var check_global = require('./check_global.js')
+
+exports.usage = "检查项目内容";
+
+exports.set_options = function( optimist ){
+
+    optimist.alias('g','global')
+    return optimist.describe('g','检查当前目录下所有 .js 文件的全局变量')
+
+}
+
+exports.run = function( options ){
+
+    if( options.global ) {
+
+        utils.logger.log('全局变量检查')
+
+        utils.path.each_directory( process.cwd() , function( path ){
+            if( syspath.extname(path) !== '.js' ) return;
+            check_global.globalsIdentifierCheck( path , function( err , output ){
+                if( err || !output || !output.length ) return
+
+                console.info( path );
+                console.info( "=============================" );
+
+                console.info( output.join('\n') )
+
+                console.info("")
+                console.info("")
+            })
+        } , true );
+
+    }
+
+    utils.logger.log('done.')
+
+}
